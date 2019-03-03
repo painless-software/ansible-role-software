492131ca73cf07d08b61a340c33eacaff4f7505d
Peter Bittner <peter@painless.software>
2019-03-04 00:22:18 +0100

    GNOME tool needs IDs instead of numbers

--- files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
+++ files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
-echo 'Activate useful GNOME shell extensions ...'
-
 # - https://extensions.gnome.org/extension/2/move-clock/
 # - https://extensions.gnome.org/extension/708/panel-osd/
 # - https://extensions.gnome.org/extension/873/overview-all-windows/
 # - https://extensions.gnome.org/extension/1015/gravatar/
 # - https://extensions.gnome.org/extension/1065/docker-status/
 
-for extension in 2 708 873 1015 1065; do
+# extension names = folder names from /usr/local/share/gnome-shell/extensions/
+DISABLE_EXTENSIONS="\
+    Move_Clock@rmy.pobox.com \
+    panel-osd@berend.de.schouwer.gmail.com \
+    OverviewAllWindows@amiller27 \
+    gravatar@jr.rlabs.io \
+    docker_status@gpouilloux"
+
+echo 'Ensure unwanted GNOME Shell extensions are disabled ...'
+for extension in ${DISABLE_EXTENSIONS}; do
     gnome-shell-extension-tool --disable-extension "$extension"
 done
 
@@ -20,6 +27,22 @@ done
 # - https://extensions.gnome.org/extension/1108/add-username-to-top-panel/
 # - https://extensions.gnome.org/extension/1143/logout-button/
 
-for extension in 120 723 744 779 800 1108 1143; do
+# extension names = folder names from /usr/local/share/gnome-shell/extensions/
+ENABLE_EXTENSIONS="\
+    system-monitor@paradoxxx.zero.gmail.com \
+    pixel-saver@deadalnix.me \
+    Hide_Activities@shay.shayel.org \
+    clipboard-indicator@tudmotu.com \
+    remove-dropdown-arrows@mpdeimos.com \
+    add-username-toppanel@brendaw.com \
+    LogOutButton@kyle.aims.ac.za"
+
+echo 'Ensure desired GNOME Shell extensions are enabled ...'
+for extension in ${ENABLE_EXTENSIONS}; do
     gnome-shell-extension-tool --enable-extension "$extension"
 done
+
+echo 'Restart GNOME Shell to activate changes ...'
+dbus-send --session --type=method_call \
+          --dest=org.gnome.Shell /org/gnome/Shell \
+          org.gnome.Shell.Eval string:"global.reexec_self();"
