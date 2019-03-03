a497c51d2445af8399ed996942913e8f74ecfb23
Peter Bittner <peter@painless.software>
2019-03-03 20:09:53 +0100

    Install GNOME Shell extensions system-wide
    Replace gnome-shell-extension-installer by gnome-shell-extension-manage

--- files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
+++ files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
-echo 'Installing additional GNOME shell extensions ...'
+echo 'Activate useful GNOME shell extensions ...'
 
 # - https://extensions.gnome.org/extension/2/move-clock/
-# - https://extensions.gnome.org/extension/120/system-monitor/
 # - https://extensions.gnome.org/extension/708/panel-osd/
+# - https://extensions.gnome.org/extension/873/overview-all-windows/
+# - https://extensions.gnome.org/extension/1015/gravatar/
+# - https://extensions.gnome.org/extension/1065/docker-status/
+
+for extension in 2 708 873 1015 1065; do
+    gnome-shell-extension-tool --disable-extension "$extension"
+done
+
+# - https://extensions.gnome.org/extension/120/system-monitor/
 # - https://extensions.gnome.org/extension/723/pixel-saver/
 # - https://extensions.gnome.org/extension/744/hide-activities-button/
 # - https://extensions.gnome.org/extension/779/clipboard-indicator/
 # - https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
-# - https://extensions.gnome.org/extension/873/overview-all-windows/
-# - https://extensions.gnome.org/extension/1015/gravatar/
-# - https://extensions.gnome.org/extension/1065/docker-status/
 # - https://extensions.gnome.org/extension/1108/add-username-to-top-panel/
 # - https://extensions.gnome.org/extension/1143/logout-button/
 
-for extension in 2 120 708 723 744 779 800 873 1015 1065 1108 1143; do
-    gnome-shell-extension-installer "$extension" --update --yes
+for extension in 120 723 744 779 800 1108 1143; do
+    gnome-shell-extension-tool --enable-extension "$extension"
 done
