b27653c1275b94ed7a693fec440f0873cbdd4496
Peter Bittner <peter@painless.software>
2021-09-27 12:11:57 +0200

    Don't enforce Unity style in userdefaults script by default

--- files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
+++ files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env sh
 
 # - https://extensions.gnome.org/extension/2/move-clock/
+# - https://extensions.gnome.org/extension/120/system-monitor/
 # - https://extensions.gnome.org/extension/708/panel-osd/
+# - https://extensions.gnome.org/extension/723/pixel-saver/
+# - https://extensions.gnome.org/extension/744/hide-activities-button/
+# - https://extensions.gnome.org/extension/779/clipboard-indicator/
+# - https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
 # - https://extensions.gnome.org/extension/873/overview-all-windows/
 # - https://extensions.gnome.org/extension/1015/gravatar/
 # - https://extensions.gnome.org/extension/1065/docker-status/
+# - https://extensions.gnome.org/extension/1108/add-username-to-top-panel/
+# - https://extensions.gnome.org/extension/1143/logout-button/
 
-# extension names = folder names from /usr/local/share/gnome-shell/extensions/
+# NOTE: extension names = folder names from /usr/local/share/gnome-shell/extensions/
+
 DISABLE_EXTENSIONS="\
     Move_Clock@rmy.pobox.com \
     panel-osd@berend.de.schouwer.gmail.com \
+    pixel-saver@deadalnix.me \
     OverviewAllWindows@amiller27 \
     gravatar@jr.rlabs.io \
-    docker_status@gpouilloux"
+    docker_status@gpouilloux \
+    LogOutButton@kyle.aims.ac.za \
+    "
+ENABLE_EXTENSIONS="\
+    system-monitor@paradoxxx.zero.gmail.com \
+    Hide_Activities@shay.shayel.org \
+    clipboard-indicator@tudmotu.com \
+    remove-dropdown-arrows@mpdeimos.com \
+    add-username-toppanel@brendaw.com \
+    "
 
 echo 'Ensure unwanted GNOME Shell extensions are disabled ...'
 for extension in ${DISABLE_EXTENSIONS}; do
     gnome-shell-extension-tool --disable-extension "$extension"
 done
-
-# - https://extensions.gnome.org/extension/120/system-monitor/
-# - https://extensions.gnome.org/extension/723/pixel-saver/
-# - https://extensions.gnome.org/extension/744/hide-activities-button/
-# - https://extensions.gnome.org/extension/779/clipboard-indicator/
-# - https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
-# - https://extensions.gnome.org/extension/1108/add-username-to-top-panel/
-# - https://extensions.gnome.org/extension/1143/logout-button/
-
-# extension names = folder names from /usr/local/share/gnome-shell/extensions/
-ENABLE_EXTENSIONS="\
-    system-monitor@paradoxxx.zero.gmail.com \
-    pixel-saver@deadalnix.me \
-    Hide_Activities@shay.shayel.org \
-    clipboard-indicator@tudmotu.com \
-    remove-dropdown-arrows@mpdeimos.com \
-    add-username-toppanel@brendaw.com \
-    LogOutButton@kyle.aims.ac.za"
 
 echo 'Ensure desired GNOME Shell extensions are enabled ...'
 for extension in ${ENABLE_EXTENSIONS}; do
--- files/base/desktop/userdefaults.d/11-gnome-settings.sh
+++ files/base/desktop/userdefaults.d/11-gnome-settings.sh
@@ -3,19 +3,25 @@
 echo 'Restoring GNOME desktop organization default settings ...'
 
 # Ubuntu Unity style:
+# gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+# Windows/Mint/Cinnamon style:
+# gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '24'
+# gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+# gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor 'true'
+# gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top 'true'
+
+# application bar appearance
 gsettings set org.gnome.desktop.calendar show-weekdate 'true'
 gsettings set org.gnome.desktop.interface clock-show-date 'false'
 gsettings set org.gnome.desktop.interface show-battery-percentage 'true'
-gsettings set org.gnome.desktop.peripherals.mouse natural-scroll 'true'
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click 'false'
-gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+# desktop appearanche & behavior
 gsettings set org.gnome.nautilus.desktop trash-icon-visible 'false'
 gsettings set org.gnome.shell enable-hot-corners 'true'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed 'false'
 
-# Windows/Mint/Cinnamon style:
-# gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '24'
-# gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed 'true'
-# gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
-# gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor 'true'
-# gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top 'true'
+# device behavior
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll 'true'
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll 'true'
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click 'true'
