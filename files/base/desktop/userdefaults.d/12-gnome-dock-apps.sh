c913e3bb8e23d030e933dd4f97a71217e54b4d6c
Peter Bittner <peter@painless.software>
2023-09-24 21:26:02 +0200

    Update apps list in GNOME dock

--- files/base/desktop/userdefaults.d/12-gnome-dock-apps.sh
+++ files/base/desktop/userdefaults.d/12-gnome-dock-apps.sh
@@ -3,12 +3,12 @@
 # HINT: run `gsettings get org.gnome.shell favorite-apps` for current config
 dock_apps="[
     'org.gnome.Nautilus.desktop',
-    'chromium-browser.desktop',
-    'firefox.desktop',
+    'firefox_firefox.desktop',
+    'google-chrome.desktop',
     'org.gnome.Geary.desktop',
     'org.gnome.Terminal.desktop',
-    'codium.desktop',
-    'rhythmbox.desktop'
+    'codium_codium.desktop',
+    'org.gnome.Rhythmbox3.desktop'
 ]"
 
 echo 'Set GNOME dock application shortcuts ...'
