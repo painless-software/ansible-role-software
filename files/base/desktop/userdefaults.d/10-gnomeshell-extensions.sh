d5ebf5def436723a7a23a7f7c28b21613cf89215
Peter Bittner <peter@painless.software>
2023-09-24 18:40:05 +0200

    Update extension list also in userdefaults script

--- files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
+++ files/base/desktop/userdefaults.d/10-gnomeshell-extensions.sh
@@ -1,35 +1,21 @@
 #!/usr/bin/env sh
 
-# - https://extensions.gnome.org/extension/2/move-clock/
-# - https://extensions.gnome.org/extension/120/system-monitor/
-# - https://extensions.gnome.org/extension/708/panel-osd/
-# - https://extensions.gnome.org/extension/723/pixel-saver/
 # - https://extensions.gnome.org/extension/744/hide-activities-button/
-# - https://extensions.gnome.org/extension/779/clipboard-indicator/
-# - https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
-# - https://extensions.gnome.org/extension/873/overview-all-windows/
-# - https://extensions.gnome.org/extension/1015/gravatar/
-# - https://extensions.gnome.org/extension/1065/docker-status/
 # - https://extensions.gnome.org/extension/1108/add-username-to-top-panel/
-# - https://extensions.gnome.org/extension/1143/logout-button/
+# - https://extensions.gnome.org/extension/3010/system-monitor-next/
+# - https://extensions.gnome.org/extension/4958/clipman/
+# - https://extensions.gnome.org/extension/5103/docker/
 
 # NOTE: extension names = folder names from /usr/local/share/gnome-shell/extensions/
 
 DISABLE_EXTENSIONS="\
-    Move_Clock@rmy.pobox.com \
-    panel-osd@berend.de.schouwer.gmail.com \
-    pixel-saver@deadalnix.me \
-    OverviewAllWindows@amiller27 \
-    gravatar@jr.rlabs.io \
-    docker_status@gpouilloux \
-    LogOutButton@kyle.aims.ac.za \
+    docker@stickman_0x00.com \
     "
 ENABLE_EXTENSIONS="\
-    system-monitor@paradoxxx.zero.gmail.com \
     Hide_Activities@shay.shayel.org \
-    clipboard-indicator@tudmotu.com \
-    remove-dropdown-arrows@mpdeimos.com \
     add-username-toppanel@brendaw.com \
+    system-monitor-next@paradoxxx.zero.gmail.com
+    clipman@popov895.ukr.net \
     "
 
 echo 'Ensure unwanted GNOME Shell extensions are disabled ...'
