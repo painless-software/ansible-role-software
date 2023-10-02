c00bdbaa7ef722c9859b3687c205dbcb4b9fa84a
Peter Bittner <peter@painless.software>
2023-10-02 05:55:18 +0200

    Automatically configure Git username (via userdefaults)

--- files/base/desktop/userdefaults.d/40-home-skel-config.sh
+++ files/base/desktop/userdefaults.d/40-home-skel-config.sh
@@ -2,3 +2,9 @@
 
 echo 'Restore user files provided by skeleton /etc/skel/.config/ ...'
 cp -rv /etc/skel/.config ${HOME}/
+
+if which git > /dev/null ; then
+    echo 'Updating global Git configuration ...'
+    git config --global user.name "$(getent passwd ${USER} | cut -d: -f5 | cut -d, -f1)"
+    git config --global --list
+fi
